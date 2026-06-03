92fbcfcd890771cd933bec9fa06c2495a50d889e
Yuvraj <theyuvrajtayal21@gmail.com>
2026-06-03 16:01:22 +0530

    day 1 project smaller one but started

---
+++ .Day_1_project.py
@@ -0,0 +1,4 @@
+print("Welcome to the brand name generator")
+city = input("Which city do you live in?\n")
+pet = input("What is the name of your pet\n")
+print("Your brand name could be = " + city + " " + pet)
